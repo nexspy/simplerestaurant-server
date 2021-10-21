031b2f66bcda68d16a18594adc1343cc1838d36a
Number 4 <nirmalmaswa@live.com>
2021-10-21 11:16:44 +0545

    food item delete code added

--- controllers/FoodController.js
+++ controllers/FoodController.js
@@ -4,14 +4,14 @@ import mongoose from "mongoose";
 import FoodModel from '../models/Food.js';
 
 export const getFoodItems = async (req, res) => {
-    try {
-        var success = false;
-        var message = "no product found";
+    var success = false;
+    var message = "no food item found";
 
+    try {
         const foods = await FoodModel.find();
 
         success = true;
-        message = "loaded product successfully";
+        message = "loaded food item successfully";
         if (foods.length <= 0) {
             message = "no food found";
         }
@@ -31,9 +31,10 @@ export const getFoodItems = async (req, res) => {
 }
 
 export const saveFoodItem = async (req, res) => {
+    var success = false;
+    var message = "could not save the food item";
+
     try {
-        var success = false;
-        var message = "could not save the food item";
         const body = req.body;
 
         const food = new FoodModel(body);
@@ -41,6 +42,42 @@ export const saveFoodItem = async (req, res) => {
 
         success = true;
         message = "food item was created successfully";
+
+        res.status(200).json({
+            success: success,
+            message: message,
+            food: food,
+        });
+
+    } catch (error) {
+        res.status(404).json({
+            success: success,
+            message: message,
+            food: false,
+        });
+    }
+}
+
+export const deleteFoodItem = async (req, res) => {
+    var success = false;
+    var message = "could not delete the food item";
+
+    try {
+        
+        const id = req.params.foodId;
+
+        const food = await FoodModel.deleteOne({
+            _id: id,
+        });
+    
+        success = true;
+        message = "food item was deleted successfully";
+
+        if (typeof food.deletedCount !== "undefined") {
+            if (food.deletedCount <= 0) {
+                message = "no record found to delete";
+            }
+        }
 
         res.status(200).json({
             success: success,
--- routes/FoodRoutes.js
+++ routes/FoodRoutes.js
@@ -2,9 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
-import { getFoodItems, saveFoodItem } from '../controllers/FoodController.js';
+import { getFoodItems, saveFoodItem, deleteFoodItem } from '../controllers/FoodController.js';
 
 router.get('/', getFoodItems);
 router.post('/create', saveFoodItem);
+router.delete('/:foodId/delete', deleteFoodItem);
 
 export default router;
